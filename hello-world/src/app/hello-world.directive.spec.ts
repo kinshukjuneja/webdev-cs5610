58d4e0f946571773175286541d2ca3268cb8c754
Kinshuk Juneja <kinshuk.juneja02@gmail.com>
2019-01-24 19:35:57 -0800

    deleting unnecessary files

--- hello-world/src/app/hello-world.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HelloWorldDirective } from './hello-world.directive';
-
-describe('HelloWorldDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HelloWorldDirective();
-    expect(directive).toBeTruthy();
-  });
-});
